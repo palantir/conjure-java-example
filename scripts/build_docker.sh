d4fb85aeab6210a47c4775cbb901d626f34c9aa3
Qinfeng Chen <qchen@palantir.com>
2018-07-27 14:42:38 +0100

    fix working dir

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -9,9 +9,10 @@ rm -rf $DEST
 mkdir -p $DEST
 
 cp ./scripts/Dockerfile $DEST/
-cp "./recipe-example-server/build/distributions/recipe-example-server-${VERSION}.jar" $DEST/
+cp "./recipe-example-server/build/distributions/recipe-example-server-${VERSION}.tar" $DEST/
 cp ./recipe-example-server/var/conf/recipes.yml $DEST/
 
+cd $DEST
 docker build -t "palantir/recipe-example-server:$VERSION" .
 docker tag "palantir/recipe-example-server:$VERSION" "palantir/recipe-example-server:latest"
 
