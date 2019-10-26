fcc88a73bb2cfc22585a764903955e0037124d22
Carter Kozak <ckozak@ckozak.net>
2019-10-26 11:45:50 -0400

    Update RecipeBookApplicationTest.java

--- recipe-example-server/src/test/java/com/palantir/conjure/examples/RecipeBookApplicationTest.java
+++ recipe-example-server/src/test/java/com/palantir/conjure/examples/RecipeBookApplicationTest.java
@@ -17,7 +17,6 @@
 package com.palantir.conjure.examples;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
