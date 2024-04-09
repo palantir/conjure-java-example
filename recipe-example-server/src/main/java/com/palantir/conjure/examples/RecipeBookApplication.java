042dd0d34a7a89efddb0e6ee28557d863cb56ece
Brandon Marc-Aurele <bmarcaur@gmail.com>
2024-04-09 17:42:51 -0400

    Repair Gradle and Build Infrastructure (#614)

--- recipe-example-server/src/main/java/com/palantir/conjure/examples/RecipeBookApplication.java
+++ recipe-example-server/src/main/java/com/palantir/conjure/examples/RecipeBookApplication.java
@@ -16,16 +16,8 @@
 
 package com.palantir.conjure.examples;
 
-import com.google.common.collect.ImmutableSet;
 import com.palantir.conjure.examples.recipe.Configuration;
-import com.palantir.conjure.examples.recipe.api.BakeStep;
-import com.palantir.conjure.examples.recipe.api.Ingredient;
-import com.palantir.conjure.examples.recipe.api.Recipe;
 import com.palantir.conjure.examples.recipe.api.RecipeBookServiceEndpoints;
-import com.palantir.conjure.examples.recipe.api.RecipeName;
-import com.palantir.conjure.examples.recipe.api.RecipeStep;
-import com.palantir.conjure.examples.recipe.api.Temperature;
-import com.palantir.conjure.examples.recipe.api.TemperatureUnit;
 import com.palantir.conjure.examples.resources.RecipeBookResource;
 import com.palantir.conjure.java.api.config.ssl.SslConfiguration;
 import com.palantir.conjure.java.config.ssl.SslSocketFactories;
@@ -34,55 +26,34 @@ import io.undertow.Handlers;
 import io.undertow.Undertow;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Set;
 import javax.net.ssl.SSLContext;
 
 public final class RecipeBookApplication {
-
-    protected static final String KEY_STORE_PATH = "src/test/resources/certs/keystore.jks"; // password changeit
-    protected static final String TRUSTSTORE_PATH = "src/test/resources/certs/truststore.jks"; // password changeit
+    private static final String KEY_STORE_PATH = "src/test/resources/certs/keystore.jks"; // password changeit
+    private static final String TRUSTSTORE_PATH = "src/test/resources/certs/truststore.jks"; // password changeit
 
     private RecipeBookApplication() {
         // noop
     }
 
     public static void main(String[] _args) throws IOException {
-        Configuration conf = ConfigurationLoader.load();
-
+        Configuration config = ConfigurationLoader.load();
         SslConfiguration sslConfig =
                 SslConfiguration.of(Paths.get(TRUSTSTORE_PATH), Paths.get(KEY_STORE_PATH), "changeit");
         SSLContext sslContext = SslSocketFactories.createSslContext(sslConfig);
 
-        Undertow server = Undertow.builder()
-                .addHttpsListener(conf.getPort(), conf.getHost(), sslContext)
-                .setHandler(Handlers.path()
+        Undertow.Builder builder = Undertow.builder().addHttpsListener(config.getPort(), config.getHost(), sslContext);
+        setupRecipeApplicationServer(builder).start();
+    }
+
+    public static Undertow setupRecipeApplicationServer(Undertow.Builder builder) {
+        return builder.setHandler(Handlers.path()
                         .addPrefixPath(
                                 "api/",
                                 ConjureHandler.builder()
-                                        .services(RecipeBookServiceEndpoints.of(new RecipeBookResource(someRecipes())))
+                                        .services(RecipeBookServiceEndpoints.of(
+                                                new RecipeBookResource(ExampleRecipes.getCommonRecipes())))
                                         .build()))
                 .build();
-
-        server.start();
-    }
-
-    private static Set<Recipe> someRecipes() {
-        return ImmutableSet.of(
-                Recipe.builder()
-                        .name(RecipeName.of("roasted broccoli with garlic"))
-                        .steps(ImmutableSet.of(
-                                RecipeStep.chop(Ingredient.valueOf("3 cloves of garlic")),
-                                RecipeStep.mix(ImmutableSet.of(
-                                        Ingredient.of("2 tbsp extra olive oil"), Ingredient.of("chopped garlic"))),
-                                RecipeStep.bake(BakeStep.of(Temperature.of(230, TemperatureUnit.CELSIUS), 1200))))
-                        .build(),
-                Recipe.builder()
-                        .name(RecipeName.of("baked potatoes"))
-                        .steps(ImmutableSet.of(
-                                RecipeStep.mix(ImmutableSet.of(
-                                        Ingredient.of("rub oil all over the potatoes"),
-                                        Ingredient.of("Rub salt all over the potatoes"))),
-                                RecipeStep.bake(BakeStep.of(Temperature.of(220, TemperatureUnit.CELSIUS), 2700))))
-                        .build());
     }
 }
